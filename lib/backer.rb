284955451bad522b48511577266eb93c5e8d6944
Aaron Goen <aarongoen2.0@gmail.com>
2020-09-03 16:03:06 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,17 @@
 class Backer 
 
-attr_reader :backed_projects, :name 
+attr_accessor :project 
+attr_reader :backed_projects, :name
 
 def initialize(name)
   @backed_projects = [] 
   @name = name
 end
+
+def back_project 
+  pro
+  
+end 
+
 
 end 
